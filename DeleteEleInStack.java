2415d0e83a91720b3055292cad46bf37c7d2b7f2
Gopal Verma <iasgopalverma9621@gmail.com>
2026-04-01 23:34:38 +0530

    Delete Element From Stack

---
+++ DeleteEleInStack.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DeleteEleInStack {
+    public static void main(String[] args){
+        Stack<Integer> s1 = new Stack <> ();
+        
+        s1.push(10);
+        s1.push(20);
+        s1.push(30);
+        s1.push(40);
+        s1.pop();
+        s1.pop();
+        System.out.println(s1);
+    }
+}
